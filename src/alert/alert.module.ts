19361fc3604811cc6c912dc0253aa1353ed35f21
njajkes <naikes.nikolait@yandex.ru>
2023-07-01 04:02:48 +0300

    chore: codestyle fixes

--- src/alert/alert.module.ts
+++ src/alert/alert.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AlertService } from './alert.service';
 import { AlertController } from './alert.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   providers: [AlertService],
-  controllers: [AlertController]
+  controllers: [AlertController],
 })
 export class AlertModule {}
